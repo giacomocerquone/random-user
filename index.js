b3be72edacff4f5b05d65b9fa4e914f0b5f2f4c6
Giacomo Cerquone <cerquone@gmail.com>
2016-04-12 19:13:52 +0200

    Reintroduced ES6 let and arrow function to be able to minify the script with the harmony branch of uglify-js

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
 var request = require('superagent');
 
 module.exports = function(type) {
-  return new Promise( function(resolve, reject) {
+  return new Promise( (resolve, reject) => {
 
     request
       .get('http://api.randomuser.me/')
-      .end(function(err, res) {
+      .end( (err, res) => {
         if (err)
           reject(err);
 
-        var data = JSON.parse(res.text).results[0];
+        let data = JSON.parse(res.text).results[0];
 
         if (type === 'simple') {
           resolve({
